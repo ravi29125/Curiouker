997cbf4f6f297c3f222c85e5820d64fa7343bcb0
ravi29125 <136157540+ravi29125@users.noreply.github.com>
2024-06-16 14:55:24 +0530

    "Latest Vercil"

--- Cypher/settings.py
+++ Cypher/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-__tdx(n72-m)il@)kln5)!wdsden#vmj8081*0!qf*=bvn2gi#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
